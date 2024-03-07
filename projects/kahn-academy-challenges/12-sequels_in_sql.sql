36c7ffbb88ae2911d880de831b76892dc5364882
bstreck <152208816+bstreck@users.noreply.github.com>
2024-03-07 00:21:03 -0600

    Create 12-sequels_in_sql.sql
    Kahn academy lesson 3, challenge 3

---
+++ projects/kahn-academy-challenges/12-sequels_in_sql.sql
@@ -0,0 +1,4 @@
+##We've created a table with all the 'Harry Potter' movies, with a sequel_id column 
+that matches the id of the sequel for each movie. 
+
+##Issue a  SELECT that will show the title of each movie next to its sequel's title (or NULL if it doesn't have a sequel)
