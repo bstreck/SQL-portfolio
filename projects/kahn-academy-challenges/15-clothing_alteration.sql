60452ea7ab48aed9bfa9b27a153f047b9d668741
bstreck <152208816+bstreck@users.noreply.github.com>
2024-03-07 00:25:28 -0600

    Create 15-clothing_alteration.sql

---
+++ projects/kahn-academy-challenges/15-clothing_alteration.sql
@@ -0,0 +1,6 @@
+##We've created a database of clothes, and decided we need a price column.  Use ALTER to add a 'price' column to the table.  Then select all the columns in each row to see what your table looks like now.
+
+##Now assign each item a price, using UPDATE - item 1 should be 10 dollars, item 2  should be 20 dollars, item 3 should be 30 dollars. When you're done, do another  SELECT of all the rows to check that it worked as expected
+
+##Now insert a new item into the table that has all three attributes filled in,  including 'price'. Do one final SELECT of all the rows to check it worked
+
