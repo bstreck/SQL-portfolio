ddd847c90e75fcfe780c18dcd12c557b33d893e9
bstreck <152208816+bstreck@users.noreply.github.com>
2024-03-07 00:22:19 -0600

    Create 13-friendbook.sql

---
+++ projects/kahn-academy-challenges/13-friendbook.sql
@@ -0,0 +1,6 @@
+##We've created a database for a friend networking site, with a table storing data on each person, a table on each person's hobbies, and a table of friend connections between the people.
+
+##Use a JOIN to display a table showing people's names with their hobbies.
+
+##Use another SELECT with a  JOIN to show the names of each pair of friends, based on the data in the friends table.
+
