c192d655b3179cc0b4350926bf6ad7ca91bee74f
bstreck <152208816+bstreck@users.noreply.github.com>
2024-03-07 00:23:36 -0600

    Create 14-dynamic_documents.sql

---
+++ projects/kahn-academy-challenges/14-dynamic_documents.sql
@@ -0,0 +1,6 @@
+##We've created a database for a documents app, with rows for each document with it's 
+title, content, and author.
+
+##Use UPDATE to change the author to 'Jackie Draper' for all rows where it's currently 'Jackie Paper'. Then re-select all the rows to make sure the table changed like you expected.
+
+##Now you'll delete a row, being very careful not to delete all the rows. Only delete  the row where the title is 'Things I'm Afraid Of'. Then re-select all the rows to make sure the table changed like you expected.
