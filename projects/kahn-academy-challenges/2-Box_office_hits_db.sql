e24bea2e0f64d0200d71389508683713c02b6cfb
bstreck <152208816+bstreck@users.noreply.github.com>
2024-03-06 23:52:36 -0600

    Update 2-Box_office_hits_db.sql

--- projects/kahn-academy-challenges/2-Box_office_hits_db.sql
+++ projects/kahn-academy-challenges/2-Box_office_hits_db.sql
@@ -8,10 +8,12 @@
   INSERT INTO movies VALUES (4, "Shrek 2", 2004);
   INSERT INTO movies VALUES (5, "The Lion King", 1994);
   INSERT INTO movies VALUES (6, "Disney's Up", 2009);
- 
+
+
 ##Select all of the movies in the database
   SELECT *
   FROM movies;
+
 
 ##Select only the movies that were released in the year 2000 or later, not before. Sort the results so that the earlier movies are listed first.
   SELECT *
