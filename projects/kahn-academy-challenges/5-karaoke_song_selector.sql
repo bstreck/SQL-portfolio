b6bd54e5817b3ea35bf82e4d1b38c8bb99278f57
bstreck <152208816+bstreck@users.noreply.github.com>
2024-03-06 23:56:11 -0600

    Update 5-karaoke_song_selector.sql

--- projects/kahn-academy-challenges/5-karaoke_song_selector.sql
+++ projects/kahn-academy-challenges/5-karaoke_song_selector.sql
@@ -1,4 +1,4 @@
-## We've created a table with songs, and in this challenge, you'll use queries to decide what songs to sing.
+## We've created a table with songs, and in this challenge, you'll use queries to decide what songs to sing.##
 
   CREATE TABLE songs (
       id INTEGER PRIMARY KEY,
@@ -8,7 +8,7 @@
       duration INTEGER,
       released INTEGER);
       
-  INSERT INTO songs (title, artist, mood, duration, released)
+ INSERT INTO songs (title, artist, mood, duration, released)
       VALUES ("Bohemian Rhapsody", "Queen", "epic", 60, 1975);
   INSERT INTO songs (title, artist, mood, duration, released)
       VALUES ("Let it go", "Idina Menzel", "epic", 227, 2013);
@@ -24,4 +24,17 @@
       VALUES ("Sweet Caroline", "Neil Diamond", "happy", 201, 1969);
   INSERT INTO songs (title, artist, mood, duration, released)
       VALUES ("Call me maybe", "Carly Rae Jepsen", "happy", 193, 2011);
-    
+
+
+## Select the titles of the songs that have an 'epic' mood or a release date after 1990##
+  
+  SELECT title
+  FROM songs
+  WHERE released > 1990 OR mood = 'epic';
+
+
+##Select titles of songs that are 'epic', and released after 1990, and less than 4 minutes long##
+  
+  SELECT title
+  FROM songs
+  WHERE released >1990 AND mood = 'epic' AND duration < 240; 
